e210b8938aeede7897ff673d9e611f19f69d065c
shreepals23 <ssripal424@gmail.com>
2025-11-08 16:38:07 +0530

    hello world feature added

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   {/* <App/> */}
+   <h1>hello world devolped by shreepal</h1>
   </React.StrictMode>
 );
 
