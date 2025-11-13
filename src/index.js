aab00ac6fbedfd3125295a63c21c152353fc6312
shreepals23 <ssripal424@gmail.com>
2025-11-13 16:11:12 +0530

    react-components-state-events

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import ConstructorImplementation from './react-component/class-component/ConstructorImplement';
+// import ExampleClassComponent from './react-component/class-component/ExampleClassComponent';
+// import FunctionComponent from './react-component/function-component/FunctionComponent';
+// import ButtonClickEvent from './react-event/ButtonClickEvent';
+// import ClasscomponentStateManagement from './react-state/cc/ClasscomponentStateManagement';
+import FunctionComponent from './react-state/fc/FunctionComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h4>FSD-RJS SARAVANAN BATCH</h4>
-    <ol>
-      <li>Laxmi Roshini</li>
-      <li>Pavithra</li>
-      <li>Jesintha</li>
-      <li>Shenbagam</li>
-      <li>Prem Kumar</li>
-      <li>Marimuthu</li>
-      <li>Shreepal</li>
-    </ol>
+    {/* <ConstructorImplementation/> */}
+    {/* <ExampleClassComponent/> */}
+    {/* <FunctionComponent/> */}
+    {/* <ButtonClickEvent/> */}
+    {/* <ClasscomponentStateManagement/> */}
+    <FunctionComponent/>
+
+
+
+
+
+
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
