73eef9e1c7d6ec5fb75d00cb2e8c0dbfbdba1486
Shreepal S <146174524+shreepals23@users.noreply.github.com>
2025-11-20 12:09:09 +0530

    Uncomment Bootstrap and component imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import Button from './react-bootstrap/Button';
-// import Product from "./react-css/AboutPage/Product";
-// import MovieCard from './react-css/inline-css/MovieCard';
-// import GalleryPage from './react-css/internal-css/GalleryPage';
+
+//bootstrap and js import
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import Button from './react-bootstrap/Button';
+import Product from "./react-css/AboutPage/Product";
+import MovieCard from './react-css/inline-css/MovieCard';
+import GalleryPage from './react-css/internal-css/GalleryPage';
 import ProfileCard from './react-module-css/ProfileCard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
