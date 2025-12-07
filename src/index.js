cef2a7e9503a0e9ec03078e8f53d61624552ca46
saravanan.mca1992@gmail.com <53892651+saravanan-prog@users.noreply.github.com>
2025-12-07 21:09:32 +0530

    usememo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import CounterApp from './pages/react-before-usememo/CounterApp';
-import CounterAppusememo from './pages/react-before-usememo/CounterAppusememo';
+import CounterAppusememo from './pages/react-after-usememo/CounterAppusememo';
 
 
 
