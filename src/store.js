033f4ab59f8a1946c3b89eb476a9e403c169af8c
saravanan.mca1992@gmail.com <53892651+saravanan-prog@users.noreply.github.com>
2025-12-18 09:07:19 +0530

    basic setup commit

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import couterPageReducer from './pages/counter/slice/counterpageSlice'
-
-export const store = configureStore({
-    reducer:{
-        counter:couterPageReducer
-    }
-})
